000ed0b21c01d64e50f7934ca019a67b46a6bc90
toprak1212 <128628316+toprak1212@users.noreply.github.com>
2024-03-11 23:49:19 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
-var http = require('http');
-http.createServer(function (req, res) {
+var http = require("http");
 
-var xip = '';
-
-  
-http.get({'host': 'api.ipify.org', 'port': 80, 'path': '/'}, function(resp) {
-  resp.on('data', function(ip) {
-    xip = ip
+var xip = "";
+http.get({ host: "api.ipify.org", port: 80, path: "/" }, function (resp) {
+  resp.on("data", function (ip) {
+    xip = ip;
   });
 });
-    
-    console.log(`Just got a request at ${req.url}!`)
+http
+  .createServer(function (req, res) {
+    console.log(`Just got a request at ${req.url}!`);
     res.write(ip);
     res.end();
-}).listen(process.env.PORT || 3000);
+  })
+  .listen(process.env.PORT || 3000);
