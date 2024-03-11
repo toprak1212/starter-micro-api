a3cb1f0125f79e35f505c889c70b2f092d06afcc
toprak1212 <128628316+toprak1212@users.noreply.github.com>
2024-03-11 23:44:32 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 var http = require('http');
 http.createServer(function (req, res) {
+
+
+http.get({'host': 'api.ipify.org', 'port': 80, 'path': '/'}, function(resp) {
+  resp.on('data', function(ip) {
+    console.log("My public IP address is: " + ip);
+  });
+});
+    
     console.log(`Just got a request at ${req.url}!`)
     res.write('RX!');
     res.end();
